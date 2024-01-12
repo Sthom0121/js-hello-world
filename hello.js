3e35683bc37b2b9901c40b993c79cf2a4afc3121
Sthom0121 <sharenee1979@yahoo.com>
2024-01-11 20:00:54 -0600

    test

--- hello.js
+++ hello.js
@@ -4,7 +4,19 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+//console.log(Number('33'));
+//const input = require('readline-sync');
+
+//let info = input.question("Please enter your age: ");
+//The users enters 25.
+
+//console.log(typeof info);
+const input = require('readline-sync');
+
+let info = input.question("Please enter your age: ");
+//The user enters 25.
+
+console.log(typeof info)
 
 /*  
 Next, PLAY! 
